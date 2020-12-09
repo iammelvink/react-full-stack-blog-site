24b24128248ea4648b28679abb75988bf51f3da7
Melvin Kisten <iammelvink@gmail.com>
2020-12-09 18:46:16 +0200

    created basic app component

--- blog-site-frontend/src/App.js
+++ blog-site-frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* This is a prop that handles the HomePage route */}
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
   );
 }
 
---
+++ blog-site-frontend/src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const HomePage = () => (
+    /**
+     * React fragments enables a component to 
+     * return multiple elements at the same level
+     * Long hand/way
+     * < React.Fragment >
+     * </React.Fragment>
+     * 
+     * Short hand/way
+     * <>
+     * </>
+     */
+
+    <>
+        <h1> Hello, this is my awesome blog! </h1>
+        <p>
+            This is my awesome blog, consectetur adipiscing elit.Fusce lobortis diam a nisi commodo, nec commodo arcu consequat.Morbi at metus vitae justo blandit luctus sit amet vel ligula.Phasellus nec mattis metus, vitae mollis dui.Mauris aliquet consectetur lacus.Sed est orci, aliquam at vestibulum aliquam, sagittis ut metus.Ut sed ultrices augue, eu bibendum dolor.Nam varius lectus sit amet nunc accumsan posuere.Ut posuere velit eget odio viverra, rhoncus ornare augue tempus.
+        </p>
+        <p>
+            Nulla ut eleifend turpis, at ultrices nisi.Fusce lobortis diam a nisi commodo, nec commodo arcu consequat.Morbi at metus vitae justo blandit luctus sit amet vel ligula.Phasellus nec mattis metus, vitae mollis dui.Mauris aliquet consectetur lacus.Sed est orci, aliquam at vestibulum aliquam, sagittis ut metus.Ut sed ultrices augue, eu bibendum dolor.Nam varius lectus sit amet nunc accumsan posuere.Ut posuere velit eget odio viverra, rhoncus ornare augue tempus.
+        </p>
+        <p>
+            Mauris aliquet consectetur lacus.Sed est orci, aliquam at vestibulum aliquam, sagittis ut metus.Ut sed ultrices augue, eu bibendum dolor.Nam varius lectus sit amet nunc accumsan posuere.Ut posuere velit eget odio viverra, rhoncus ornare augue tempus.
+        </p>
+    </>
+);
+
+export default HomePage;
